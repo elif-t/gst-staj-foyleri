be5bbd01bb4c02b1ae4ceddb465d5d7bb0be83e5
Elif <62542563+elif-t@users.noreply.github.com>
2022-10-01 08:37:29 +0300

    Update foy4.v

--- verilog/foy4.v
+++ verilog/foy4.v
@@ -1,12 +1,12 @@
-module foy4(input wire clk, reset, d,
+module foy4(input wire clk, reset, d,a,b,
             output reg x_var);
             
       reg current_state, next_state;
       parameter state_A = 1'b0,
                 state_B = 1'b1;
       always @ (posedge clk or negedge reset) 
-      begin: STATE_MEMORY 
-        if (reset)  
+      begin 
+        if (!reset)  
           current_state <= state_A; 
         else  
           current_state <= state_B; 
@@ -26,23 +26,20 @@ module foy4(input wire clk, reset, d,
             default : next_state = state_A;
           endcase
         end
-      //a = 0 b = 1
+      
       always @ (current_state or d)
         begin: OUTPUT_LOGIC 
           case (current_state)  
             state_A :  if (d == 1'b0)
-                          x_var = 1'b0;
+                          x_var = a;
                         else   
-                          x_var = 1'b1;
-            state_B :   if (d == 1'b1)   
-                          x_var = 1'b0; 
+                          x_var = b;
+            state_B :   if (d == 1'b0)   
+                          x_var = b; 
                         else   
-                          x_var = 1'b0;
+                          x_var = a;
             default : x_var = 1'b0;
           endcase 
         end
-    initial begin
-    $display("x = %b",x_var);
-    end
 
 endmodule
