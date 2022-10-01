88b3c77279188927948210e4c4a2121501d80980
Elif <62542563+elif-t@users.noreply.github.com>
2022-10-01 07:57:31 +0300

    Update foy4_2.v

--- verilog/foy4_2.v
+++ verilog/foy4_2.v
@@ -73,38 +73,3 @@ module pattern_recog(input datain, clk,
 
 endmodule
 
-/*
-test bench
-module tb;
-  reg clk, in, rstn;
-  wire out;
-  integer l_dly;
-
-  always #10 clk = ~clk;
-
-  det_110101 u0 ( .clk(clk), .rstn(rstn), .in(in), .out(out) );
-
-  initial begin
-  	clk <= 0;
-    rstn <= 0;
-    in <= 0;
-
-    repeat (5) @ (posedge clk);
-    rstn <= 1;
-
-    @(posedge clk) in <= 1;
-    @(posedge clk) in <= 1;
-    @(posedge clk) in <= 0;
-    @(posedge clk) in <= 1;
-    @(posedge clk) in <= 0;
-    @(posedge clk) in <= 1;
-    @(posedge clk) in <= 1;
-    @(posedge clk) in <= 1;
-    @(posedge clk) in <= 0;
-    @(posedge clk) in <= 1;
-    @(posedge clk) in <= 0;
-    @(posedge clk) in <= 1;
-
-    #100 $finish;
-  end
-endmodule
