7f01d03ea78a76dbb346db928d11f6743cf81372
Elif <62542563+elif-t@users.noreply.github.com>
2022-10-02 17:01:17 +0300

    Update foy3.v

--- verilog/foy3.v
+++ verilog/foy3.v
@@ -1,36 +1,17 @@
 
-module counter(clk,SW,leds);
-  input clk,SW;
-  output[7:0] leds;
+module counter(clk, SW, leds);
 
-  tff tf1(leds[0],clk,SW);
-  tff tf2(leds[1],leds[0],SW);
-  tff tf3(leds[2],leds[1],SW);
-  tff tf4(leds[3],leds[2],SW);
-  tff tf5(leds[4],leds[3],SW);
-  tff tf6(leds[5],leds[4],SW);
-  tff tf7(leds[6],leds[5],SW);
-  tff tf8(leds[7],leds[6],SW);
-  
-endmodule
+    input clk, SW;
+    output[7:0] leds;
+    reg[7:0] cnt;
 
-module tff(clk,SW,leds);
-  input clk,SW;
-  output leds;
-  wire d;
-  dff df1(leds,d,clk,SW);
-  not n1(d,leds);
-endmodule
-
-module dff(leds,d,clk,SW);
-  input d,clk,SW;
-  output leds;
-  reg leds; // store the output value
-  always @(posedge clk)
-   begin
-    if(SW) 
-        leds=d;
-    else 
-        leds=d;
+    always@(posedge clk) begin
+      if(SW)
+        cnt <= cnt + 1;
+      else
+        cnt <= cnt - 1;
     end
+    assign leds = cnt;
+
 endmodule
+  
